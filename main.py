7f2c70a34fdd367db6832be6f44029cbe17cf97a
Garth Kidd <garth@garthk.com>
2022-07-04 21:31:44 +1000

    attempt manual reading of microphone level

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 # measure how fast we can measure sound level
 # 24000 microseconds = 40 times per second
-
+# measuring only the delay = same ahaaaa
+# so yeah can't rely on that
+# input.sound_level doesn't change outside forever?
+# try https://www.youtube.com/watch?v=E3PklpEB6lU
+# yeah nah
 def on_button_pressed_a():
-    basic.show_number(loud)
-    basic.clear_screen()
+    global loud
+    loud = 0
+    pins.digital_write_pin(28, 1)
+    for index in range(100):
+        loud = pins.analog_read_pin(6)
+        serial.write_value("loud", loud)
+        control.wait_micros(80)
+    pins.digital_write_pin(28, 0)
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def on_button_pressed_b():
@@ -14,13 +24,11 @@ input.on_button_pressed(Button.B, on_button_pressed_b)
 after = 0
 us = 0
 loud = 0
-loud = input.sound_level()
 before = input.running_time_micros()
 
 def on_forever():
-    global after, us, before, loud
+    global after, us, before
     after = input.running_time_micros()
     us = after - before
     before = after
-    loud = input.sound_level()
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,21 +1,32 @@
-// measure how fast we can measure sound level
-// 24000 microseconds = 40 times per second
-input.onButtonPressed(Button.A, function () {
-    basic.showNumber(loud)
-    basic.clearScreen()
+//  measure how fast we can measure sound level
+//  24000 microseconds = 40 times per second
+//  measuring only the delay = same ahaaaa
+//  so yeah can't rely on that
+//  input.sound_level doesn't change outside forever?
+//  try https://www.youtube.com/watch?v=E3PklpEB6lU
+//  yeah nah
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    
+    loud = 0
+    pins.digitalWritePin(28, 1)
+    for (let index = 0; index < 100; index++) {
+        loud = pins.analogReadPin(6)
+        serial.writeValue("loud", loud)
+        control.waitMicros(80)
+    }
+    pins.digitalWritePin(28, 0)
 })
-input.onButtonPressed(Button.B, function () {
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
     basic.showNumber(us)
     basic.clearScreen()
 })
 let after = 0
 let us = 0
 let loud = 0
-loud = input.soundLevel()
 let before = input.runningTimeMicros()
-basic.forever(function () {
+basic.forever(function on_forever() {
+    
     after = input.runningTimeMicros()
     us = after - before
     before = after
-    loud = input.soundLevel()
 })
